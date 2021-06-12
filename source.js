95a0dbc7f4a09e044dc932a6e35bffd04f592828
Grigory Astafyev <gastafye@cisco.com>
2021-06-12 01:16:18 -0500

    source

---
+++ source.js
@@ -0,0 +1,82 @@
+'use strict'
+let webSocket
+let fileName
+
+function init() {
+  initDropZone()
+  initWebSocket()
+}
+
+function initWebSocket() {
+  webSocket = new WebSocket ('ws://10.85.107.68:9000')
+
+  webSocket.onmessage = function(event) {
+    showDownload()
+    startDownload(event.data)
+  }
+}
+
+function sendUnsignedFile(unsignedFile) {
+  webSocket.send(unsignedFile)
+}
+
+function initDropZone() {
+  let dropZone = document.getElementById('dropZone')
+  dropZone.addEventListener('dragover', function(event){
+    event.stopPropagation()
+    event.preventDefault()
+    event.dataTransfer.dropEffect = 'copy'
+  }, false)
+
+  dropZone.addEventListener('drop', function(event){
+    event.stopPropagation()
+    event.preventDefault()
+    changeDropZone()
+
+    filename = event.dataTransfer.files[0].name
+    readFile(event.dataTransfer.files[0]).then(file => {
+      sendUnsignedFile(file)
+    })
+  })
+}
+
+function readFile(file) {
+  return new Promise(resolve => {
+    let reader = new FileReader()
+
+    reader.onload = function(){
+      resolve(reader.result)
+    }
+
+    reader.readAsArrayBuffer(file)
+  })
+}
+
+function inputFilesSelected(filesList) {
+  changeDropZone()
+  fileName = filesList[0].name
+  readFile(filesList[0]).then(file => {
+    sendUnsignedFile(file)
+  })
+}
+
+function changeDropZone() {
+  dropZone.className = 'dropZone dropZoneTop noselect pointer'
+}
+
+function showInputFiles() {
+  document.getElementById('inputObject').click()
+}
+
+function showDownload() {
+  document.getElementById('download').className = 'downloadCenter noselect pointer'
+}
+
+function startDownload(signedFile) {
+  let link = document.createElement('a')
+  link.href = window.URL.createObjectURL(new Blob([signedFile], {type: 'application/xml'}))
+  link.download = fileName
+  document.body.appendChild(link)
+
+  link.click()
+}
